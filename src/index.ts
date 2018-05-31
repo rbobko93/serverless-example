8667a92688e7f3bf5d8ff780156dfe8ccbb0f9aa
rbobko <rafal.bobko@bdpint.com>
2018-05-31 13:10:36 -0400

    Changing lambda response

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const handler: Handler = ((event, context, callback) => {
     let currentTime = new Date();
     callback(null, {
         statusCode: '200',
-        body: '2'
+        body: '5'
     });
 });
 
